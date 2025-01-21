b6735a74e6319769550f3837b9373fa2d40177e8
Jose Ramon Montes Hermida <109598200+jmontes33@users.noreply.github.com>
2025-01-21 16:15:55 +0100

    feat: toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "react-hot-toast";
 
 import "./globals.css";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
+          <Toaster />
           {children}
         </body>
       </html>
